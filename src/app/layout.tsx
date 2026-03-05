56fbf9b53028384a83361f8d3c76e26e86fd566d
Bangga Sumarna <tugasbangga@gmail.com>
2026-03-06 04:27:47 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${syne.variable} ${manrope.variable} antialiased selection:bg-white selection:text-black`}>
+        <meta name="google-site-verification" content="JZAa9rO3Q9FBm4kFbaV4t6LVl6160pd9UycrzoHLyok" />
         <LanguageProvider>
           <SmoothScrolling>
             <Preloader />
